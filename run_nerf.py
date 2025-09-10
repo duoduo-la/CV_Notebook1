909e88f0982da9b8e7604bedc017ed4efdf71ae9
duoduo-la <2738795382@qq.com>
2025-09-09 22:16:15 -0500

    second push

--- run_nerf.py
+++ run_nerf.py
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 from run_nerf_helpers import *
 
 from load_llff import load_llff_data
-from load_deepvoxels import load_dv_data
+# from load_deepvoxels import load_dv_data
 from load_blender import load_blender_data
-from load_LINEMOD import load_LINEMOD_data
+# from load_LINEMOD import load_LINEMOD_data
 import cv2
 import logging
 from datetime import datetime
